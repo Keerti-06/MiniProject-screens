b01d3f863ac65696e6091e3f46fd2a2d6f358292
iamtim@946 <kirtiathani2006@gmail.com>
2026-03-19 12:07:46 +0530

    Kirti: Added landing page with animation!

--- user/frontend/app/_layout.js
+++ user/frontend/app/_layout.js
@@ -2,7 +2,13 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
+    <Stack 
+      screenOptions={{ 
+        headerShown: false,
+        animation: 'fade', // Makes the transition to login look smooth
+      }} 
+    >
+      <Stack.Screen name="index" />
       <Stack.Screen name="(auth)" />
       <Stack.Screen name="(tabs)" />
       <Stack.Screen name="(booking)" />
